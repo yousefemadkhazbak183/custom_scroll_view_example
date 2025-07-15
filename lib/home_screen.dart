badf5a6421387cbf89ac59cd19c702c5cf3b9934
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-07-15 22:02:11 +0300

    - Finish Custom Scroll View Widget.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,45 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('Custom Scroll View'),
+            centerTitle: true,
+            pinned: true,
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+              'View item in ListView',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return Text('Item $index');
+            }, childCount: 100),
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+              'View item in GridView',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return Padding(
+                padding: EdgeInsets.all(16),
+                child: Container(height: 100, color: Colors.blue),
+              );
+            }, childCount: 50),
+            gridDelegate: (SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1,
+            )),
+          ),
+        ],
+      ),
+    );
   }
 }
